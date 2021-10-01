1e36cbbbcb4fbe97f9f2bb993da223879d70abef
Eburonia <maurice.vossen@planet.nl>
2021-10-01 16:50:10 +0000

    feat: Connect to MongoDB

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGODB_NAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def hello():
     return "hello"
+
+
+@app.route("/get_celebrities")
+def get_celebrities():
+    celebrities = mongo.db.celebrities.find()
+    return render_template("celebrities.html", celebrities=celebrities)
 
 
 if __name__ == "__main__":
