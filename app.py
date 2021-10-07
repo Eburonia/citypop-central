f418eade5257d650598c24e2022e29e005f9435c
Eburonia <maurice.vossen@planet.nl>
2021-10-07 10:11:50 +0000

    feat: Add functioning login.html page

--- app.py
+++ app.py
@@ -21,17 +21,10 @@ def hello():
     return "hello"
 
 
-@app.route("/login")
-def get_usernames():
-    usernames = mongo.db.users.find()
-    return render_template("login.html", usernames=usernames,
-                           title='Citypop Central | Login')
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check if username already exists in db
+        # check if username already exists in database
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -47,12 +40,37 @@ def register():
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-
+        session["user"] = request.form.get("username").lower()
+        flash("Regristration succesful")
+        # return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check whether username exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
