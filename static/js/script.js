aa7f7a0a81a160cec45a81886f0a281233664f96
Eburonia <maurice.vossen@planet.nl>
2021-10-08 19:36:27 +0000

    fix: toggle menu working links

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,28 @@ $('#toggle-button').on('click', function() {
 });
 
 
+$(document).on('click', '.toggle-menu-item', function(){
+    
+    let clickedItem = $('.toggle-menu-item').index(this);
+
+    switch(clickedItem) {
+
+        case 0:
+            window.open('/', '_parent');
+            break;
+
+        case 1:
+            window.open('/about', '_parent');
+            break;
+
+        case 2:
+            window.open('/contact', '_parent');
+            break;
+
+        case 3:
+            window.open('/login', '_parent');
+            break;
+          
+      } 
+
+});
