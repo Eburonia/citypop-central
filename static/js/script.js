606181a974ef9f6c149d586e7a5bb4f8985fa84f
Eburonia <maurice.vossen@planet.nl>
2021-10-26 16:34:55 +0000

    style: update form styling

--- static/js/script.js
+++ static/js/script.js
@@ -8,10 +8,10 @@ let x = Math.floor(Math.random() * 2);
 
 
 if(x == 0) {
-    $('body').css('background-image', 'url('+encodeURIComponent("/static/img/hero-imageD.jpg")+')');
+    $('body').css('background-image', 'url('+encodeURIComponent("/static/img/hero-imageF.jpg")+')');
 }
 else {
-    $('body').css('background-image', 'url('+encodeURIComponent("/static/img/hero-imageC.jpg")+')');
+    $('body').css('background-image', 'url('+encodeURIComponent("/static/img/hero-imageF.jpg")+')');
 }
 
 
