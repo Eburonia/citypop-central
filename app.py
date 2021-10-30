ca96b5795b32e7b1972cf766727bbde12104302f
Eburonia <maurice.vossen@planet.nl>
2021-10-30 08:35:17 +0000

    feat: add 404 page

--- app.py
+++ app.py
@@ -402,6 +402,11 @@ def youtube():
     return render_template("youtube.html", title=title, link=link)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+  
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
